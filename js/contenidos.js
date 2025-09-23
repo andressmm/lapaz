76ec9eac3f62296765ebfd0a0ac90e0ad6614958
andres <ed.andresmoran@gmail.com>
2025-09-23 20:57:22 -0300

    cambios varios

--- js/contenidos.js
+++ js/contenidos.js
@@ -3,7 +3,7 @@
          const contenidos = {
 
  
-      info: `   <section class="card-section">
+      info: `<section class="card-section">
         <div class="card-header">
             <h1 class="card-title">Información del Evento</h1>
             <div class="card-header-icon">👇</div>
@@ -12,7 +12,7 @@
         <div class="card-container">
             <article class="card card--infantiles">
                 <div class="card-text">
-                    <a href="?i=infantiles" class="card-link">
+                    <a href="#" onclick="clk(event);" class="card-link" id="infantiles">
                         Información Infantiles
                     </a>
                     <div class="card-date">Viernes 17/01/2026</div>
@@ -22,7 +22,7 @@
 
             <article class="card card--mayores">
                 <div class="card-text">
-                    <a href="?i=mayores" class="card-link">
+                    <a href="#" class="card-link" id='mayores' onclick="clk(event);">
                         Información Mayores
                     </a>
                     <div class="card-date">Sábado 18/01/2026</div>
@@ -57,10 +57,7 @@
         <h2>📝 Inscripción</h2>
         <p>La inscripción estará abierta hasta el 30 de septiembre. ¡No te quedes afuera!</p>
       `,
-      participantes: `
-        <h2>👥 Participantes</h2>
-        <p>Actualmente hay 120 participantes confirmados de diferentes ciudades.</p>
-      `,
+  
       cronograma: `
         <h2>📅 Cronograma</h2>
         <ul>
@@ -69,13 +66,388 @@
           <li>13:00 - Almuerzo</li>
           <li>15:00 - Premiación</li>
         </ul>
-      `
+      `,
+
+        
+      infantiles: `
+  <section class="card-section">
+        <div class="card-header">
+            <h1 class="card-title">Información de infantiles (Viernes 16/01/2026)</h1>
+            <div class="card-header-icon">👇</div>
+        </div>
+
+        <div class="summary_content">
+         <details>
+            <summary>
+             Fecha y Lugar 📌
+            </summary>
+            <div class="content">
+                <p>Viernes 17/01/2025, Puerto de La Paz</p>
+            </div>
+        </details>
+
+        
+    
+        <details>
+            <summary>
+             Cronograma 🗓️
+        </summary>
+            
+            <div class="content">
+                <br><strong>✏️ Inscripciones</strong>
+                <p>15/12/2024 - Cierre Inscripciones Online</p>
+                <br><strong>🛍️ Entrega de kits</strong>
+                <p>Jueves 16/01/2025 | 19 a 22 hs - Oficina de Turismo (Puerto La Paz)</p>
+                <p>Viernes 17/01/2025 | 8.30 a 11.30 hs - Oficina de Turismo (Puerto La Paz)</p>
+                <br><strong>🏁 Area de Transicion y Largadas</strong>
+                  <strong>Viernes 17/01/2025</strong>
+                <p><strong>12:00 Hs</strong> -
+                Habilitación Área de Transición ( todas las categorías )</p>
+                <p><strong>13:30 Hs</strong> -
+                Cierre Area de Transición</p>
+                <p><strong>14:00 Hs</strong> -
+                Largada Categorias Infantiles</p>
+                <p><strong>17:45 Hs</strong> -
+                Finalización competencias Tria Infantil</p>
+                <p><strong>18:00 Hs</strong> -
+                CEREMONIA DE ENTREGA DE PREMIOS TRIA INFANTIL en Puerto Local</p>
+            
+            </div>
+        </details>    
+
+
+        
+        <details><summary>
+            
+                 Precios de inscripcion ✅
+        </summary>
+            <div class="content">
+                <p>Precio de inscripción:</p>
+                          <p><strong>En 1 pago:</strong> $ 25.000</p> 
+                             <p><strong>Vencimiento</strong> 5 dias desde el registro. Vencido el plazo si no se registra el pago, se dará de baja la inscripcion</p>       
+          
+            </div>
+        </details>    
+
+        <details><summary>
+            
+                Categorias y distancias 🚴‍♂️
+        </summary>
+            <div class="content">
+                
+                <p><strong>PAÑALES C</strong> - 5 años (25 m nado / 1 km cic / 300 m ped)</p>
+                <p><strong>PAÑALES B</strong> - 6 años (25 m nado / 1 km cic / 300 m ped)</p>
+                    <p><strong>PAÑALES A</strong> - 7 años (25 m nado / 1 km cic / 300 m ped) </p>
+                    <p><strong>INFANTIL D</strong> - 8 años (100 m nado / 2.5 km cic / 750 m ped)</p>
+                    <p><strong>INFANTIL C</strong> - 9 años (100 m nado / 2.5 km cic / 750 m ped)</p>
+                    <p><strong>INFANTIL B</strong> - 10 años (200 m nado / 5 km cic / 1500 m ped)</p>
+                    <p><strong>INFANTIL A</strong> - 11 años (200 m nado / 5 km cic / 1500 m ped)</p>
+                    <p><strong>INFANTIL JUVENIL C</strong> - 12 años (400 m nado / 10 km cic / 2500 m ped)</p> 
+                    <p><strong>INFANTIL JUVENIL B</strong> - 13 años (400 m nado / 10 km cic / 2500 m ped)</p>
+                    <p><strong>INFANTIL JUVENIL A</strong> - 14 años (400 m nado / 10 km cic / 2500 m ped)</p>
+                    <p>__</p>
+                    <p>Se considera la edad del participante al 31/12/2024</p>
+            </div>
+        </details>    
+
+        <details><summary>
+            
+                Premiación 🏆
+        </summary>
+            <div class="content">
+                <p>El Triatlon de La Paz Infantil, no entregará premios de acuerdo a clasificación sino que todos los niños recibirán un reconocimiento por igual (medalla finisher y diploma), ya que el objetivo de este evento es promover y difundir la practica del triatlon en los niños en temprana edad y no la competencia prematura.</p>
+            </div>
+        </details>    
+
+        
+
+        <details>
+            <summary>
+            Extras 🎁
+            </summary>
+            <div class="content">
+                <p><strong>Viernes 17/01 - 21.00 Hs</strong> - Cena de Carbohidratos - Salon Polifuncional (Echague y Moreno)</p>
+
+            </div>
+        </details>
+
+
+
+        <details><summary>
+            
+                 Aceptación Menores 👨‍👩‍👦
+        </summary>
+            <div class="content">
+                <p><a class='lnk' href='infantiles.pdf'>Descargar el formulario de aceptación haciendo clic aqui</a></p>
+            </div>
+        </details>    
+</div>
+    </section>
+      `,
+
+
+
+      mayores:` 
+  <section class="card-section">
+        <div class="card-header">
+            <h1 class="card-title">Información de Mayores (Sabado 17/01/2026)</h1>
+            <div class="card-header-icon">👇</div>
+        </div>
+
+            <div class="summary_content">
+             <details>
+            <summary>
+             Fecha y Lugar 📌
+            </summary>
+            <div class="content">
+                <p>Sabado 18/01/2025, Puerto de La Paz</p>
+            </div>
+        </details>
+
+        
+    
+        <details>
+            <summary>
+             Cronograma 🗓️
+        </summary>
+            
+            <div class="content">
+                <br><strong>✏️ Inscripciones</strong>
+      
+                <p>15/12/2024 - Cierre Inscripciones Online</p>
+                <br><strong>🛍️ Entrega de kits</strong>
+                <p>Jueves 16/01/2025 | 19 a 22 hs - Oficina de Turismo (Puerto La Paz)</p>
+                <p>Viernes 17/01/2025 | 8.30 a 21 hs - Oficina de Turismo (Puerto La Paz)</p>
+                <br><strong>🏁 Area de Transicion y Largadas</strong>
+                <br>Sabado 18/01/2025
+                <p><strong>9:00 Hs</strong>
+                    Habilitación de AREA DE TRANSICION Estandar – Juveniles – Sprint - MTB - Paratriatlon
+                    </p><p><strong>11:30 Hs</strong>
+                    Cierre AREA DE TRANSICION Estandar – Juveniles – Sprint – MTB
+                </p><p><strong>14:45 Hs</strong>
+                    Traslado competidores categorías: JUVENILES – SPRINT – MTB - Paratriatlon
+                </p><p><strong class="largada">14:55 Hs</strong>
+                    Largada competidores categorías: JUVENILES – SPRINT – MTB - Paratriatlon (desde Puerto Marquez)
+                </p>
+                <p><strong>15:20 Hs</strong>
+                    Traslado competidores ESTANDAR rumbo al punto de partida
+                </p>
+                <p><strong class="largada">16:10 Hs</strong>
+                    LARGADA TRIATLON – ESTANDAR Varones y Damas (desde Paraje Barranca Negra)
+                </p>
+                <p><strong>20:00 Hs</strong>
+                    Finalización Tiempo Máximo de Competencia para ESTANDAR
+                <p><strong>22:00 Hs</strong> -
+                        CEREMONIA DE ENTREGA DE PREMIOS en Puerto Local</p>    
+                </div>
+            
+        </details>    
+
+
+        
+        <details><summary>
+                 Precios de inscripcion ✅
+        </summary>
+            <div class="content">
+            <p>Precios de inscripcion (1° Etapa) para Age Groups (Estandar) / Sprint / MTB / Juveniles / Paratriatlon:</p>
+                <p><strong>En 1 pago:</strong> $ 120.000,00</p>
+                 <p><strong>En 2 cuotas:</strong> $ 150.000,00 (dos cuotas de $ 75.000 c/u)</p>
+                 <strong>Importante:</strong> el pago total o pago de la primera cuota, se debe realizar dentro de los 5 dias de registrado. Si vencido el plazo no se registra pago total o pago de cuota, la inscripcion será dada de baja.
+                  <p>    <strong>Vencimiento 2da cuota:</strong> 20/11/2024.</p>
+            </div>
+        </details>    
+
+        <details><summary>
+            
+                 Distancias/Grupos 🏊‍♀️
+        </summary>
+            <div class="content">
+                <p>Hay 5 grupos o modalidades de competencia. Deberás elegirlo al momento de la inscripción:</p>            
+                <p><strong>JUVENILES (14 hasta 19 años)</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de ruta exclusivamente)</p>
+                <p>__</p>
+                <p><strong>MTB</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de mtb sin distincion de rodado)</p>
+                <p>__</p>
+                <p><strong>SPRINT</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de ruta exclusivamente)</p>
+                <p>__</p>
+                <p><strong>PARATRIATLON (personas con capacidades diferentes)</strong> - 750 m nado / 20 km cic / 5 km ped</p>
+                <p>__</p>
+                <p><strong>ESTANDAR</strong> - 1500 m nado / 40 km cic / 10 km ped (bicicleta de ruta exclusivamente)</p>
+
+            </div>
+        </details>    
+
+
+        
+        <details><summary>
+            
+            Categorias 🚴‍♂️
+   </summary>
+       <div class="content">
+           <p>Para todos los casos, se considerará la edad del participante al <strong>31/12/2024</strong></p>
+           <p><strong>JUVENILES (14 hasta 19 años)</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de ruta exclusivamente)</p>
+           <ul class='ul__categorias'>
+            <li>MENORES B (14 años)</li>
+            <li>MENORES A (15 años)</li>
+            <li>JUNIORS B (16-17 años)</li>
+            <li>JUNIORS A (18-19 años)</li>
+            </ul>
+           
+           
+           <p>__</p>
+           <p><strong>MTB</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de mtb sin distincion de rodado)</p>
+           <ul class='ul__categorias'>
+            <li>A (16-26 años)</li>
+            <li>B (27-37 años)</li>
+            <li>C (38-48 años)</li>
+            <li>D (49-59 años)</li>
+            <li>E (60 y + años)</li>
+            </ul>
+           <p>__</p>
+           <p><strong>SPRINT</strong> - 750 m nado / 20 km cic / 5 km ped (bicicleta de ruta exclusivamente)</p>
+           <ul class='ul__categorias'>
+            <li>A (16-26 años)</li>
+            <li>B (27-37 años)</li>
+            <li>C (38-48 años)</li>
+            <li>D (49-59 años)</li>
+            <li>E (60 y + años)</li>
+            </ul>
+            <p>__</p>
+           <p><strong>PARATRIATLON (personas con capacidades diferentes)</strong> - 750 m nado / 20 km cic / 5 km ped</p>
+           <ul class='ul__categorias'>
+            <li>PT1 - Impedimentos fisicos varios que utilizan handbike y silla de ruedas de carreras</li>
+            <li>PT2 - Impedimentos fisicos varios, con protesis</li>
+            <li>PT3 - Disminuidos visuales que necesitan de un guia durante la competencia y hacen uso de bicicleta tandem</li>
+            </ul>
+           <p>__</p>
+           <p><strong>ESTANDAR</strong> - 1500 m nado / 40 km cic / 10 km ped (bicicleta de ruta exclusivamente)</p>
+            <ul class="ul__categorias">
+                <li>A (19-24 años)</li>
+                <li>B (25-29 años)</li>
+                <li>C (30-34 años)</li>
+                <li>D (35-39 años)</li>
+                <li>E (40-44 años)</li>
+                <li>F (45-49 años)</li>
+                <li>G (50-54 años)</li>
+                <li>H (55-59 años)</li>
+                <li>I (60-64 años)</li>
+                <li>J (65-69 años)</li>
+                <li>K* (70-74 años)</li>
+                <li>L* (75-79 años)</li>
+                <li>M* (80-84 años)</li>
+                <li>N* (85 y más años)</li>
+            </ul>
+            <p>__</p>
+        <p class="red">* IMPORTANTE: En la modalidad ESTANDAR, quienes tengan mas de 70 años, participan en distancia SPRINT y largan en la prueba de SPRINT pero mantienen la categoria indicada de acuerdo a su edad.</p>
+       </div>
+   </details>    
+
+
+   <details>
+            <summary>          
+                 Reglamento tecnico 📄
+        </summary>
+            <div class="content">
+                <p><a class='lnk' href='reglamento.pdf' target='_blank'>Reglamento tecnico natacion y ciclismo</a></p>
+            </div>
+        </details>    
+
+
+
+
+   <details>
+            <summary>          
+                 Categoria ELITE 🆕
+        </summary>
+            <div class="content">
+                <p><a class='lnk' href='elite.pdf' target='_blank'>Los participantes de distancia ESTANDAR pueden optar por la nueva categoria ELITE. Toda la info aqui.</a></p>
+            </div>
+        </details>   
+
+
+
+
+   <details>
+            <summary>          
+                 Circuitos / Recorridos 📍
+        </summary>
+            <div class="content">
+                <p><a class='lnk' href='sprint.jpg' target='_blank'>Circuito distancia sprint</a></p>
+          
+                <p><a class='lnk' href='estandar.jpg' target='_blank'>Circuito distancia estandar</a></p>
+          
+            </div>
+        </details>    
+
+
+
+
+
+        <details>
+            <summary>
+            Premiación 🏆
+        </summary>
+            <div class="content">
+                <p>Se premiará del 1° al 5° en cada una de las categorias.</p>
+                <p>Medalla finisher.</p>
+                <p>No habrá premiación general.</p>
+                <p>No habrá premiación en efectivo.</p>
+                 </div>
+        </details>    
+
+
+    
+
+
+                <details>
+            <summary>          
+                 <a href='transferencias.pdf' target='_blank'>Transferencia de inscripciones</a> 🔄
+        </summary>
+        </details>
+
+
+        <details>
+            <summary>          
+                 Aceptación Menores de 15 años👨‍👩‍👦
+        </summary>
+            <div class="content">
+                <p><a class='lnk' href='infantiles.pdf' target='_blank'>Descargar el formulario de aceptación haciendo clic aqui</a></p>
+            </div>
+        </details>    
+
+
+    <details>
+            <summary>
+            Extras 🎁
+            </summary>
+            <div class="content">
+         <p><strong>Viernes 17/01 - 21.00 Hs</strong> - Cena de Carbohidratos - Salon Polifuncional (Echague y Moreno)</p>
+            </div>
+        </details>
+</div>       
+        
+        
+    </section>    
+        `
+
+
+
     };
 
+    
 
 function clk(e) {
   e.preventDefault(); // Evita el salto al #
-  const section = e.target.textContent.split(' ')[1].toLowerCase();
-  
-  document.querySelector('.main').innerHTML = contenidos[section];
+  const section = e.target.id;
+           
+    if (section==="participantes"){
+          fetch("./src/parts.html")
+        .then(res => res.text())
+        .then(data => {
+          document.querySelector(".main").innerHTML = data;
+        });
+    }
+     else 
+    {
+      document.querySelector('.main').innerHTML = contenidos[section];
+    }
 }
